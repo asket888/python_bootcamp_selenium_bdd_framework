63ce503d0a1255204d67165ca64789c77a6d04c5
Andrei Talashka <andrei.talashka@brainly.com>
2021-05-26 10:29:53 +0200

    allure report addition

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -1,3 +1,5 @@
+from allure import attach, attachment_type
+
 from pages.authentication_page import AuthenticationPage
 from pages.header_section import HeaderSection
 from utils.capabilities_util import get_driver
@@ -12,6 +14,11 @@ def before_all(context):
     context.authentication_page = AuthenticationPage(context=context)
     # open application under test
     context.authentication_page.go_to_url(url="http://automationpractice.com/index.php")
+
+
+def after_scenario(context, scenario):
+    if scenario.status == "failed":
+        attach(context.driver.get_screenshot_as_png(), attachment_type=attachment_type.PNG)
 
 
 def after_all(context):
